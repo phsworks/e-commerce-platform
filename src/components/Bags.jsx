d611c1c0be74b5c27694119a84f4b9943e64a154
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-11-26 18:54:17 +0100

    jsx

--- src/components/Bags.jsx
+++ src/components/Bags.jsx
@@ -1,10 +1,18 @@
 import "./Bags.css";
 
 const Bags = ({ bag }) => {
+
+  // const selectBag = () =>{
+  //   const bagSelected = document.querySelector(`.bag-img-div ${bag.toLowerCase()}`)
+  //   console.log(bagSelected)
+  //   bagSelected.classList.add("new-class")
+
+  // }
+
   return (
     <div className={`bag-img-div ${bag.toLowerCase()}`}>
       <p>{bag}</p>
-      <button className="bag-btn"></button>
+      <button className="bag-btn" onClick={console.log("click")}></button>
     </div>
   );
 };
