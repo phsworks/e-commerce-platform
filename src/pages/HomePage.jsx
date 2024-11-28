def3e330734b42d3d0552c1ac30245bc74a28953
Peedee01 <piethein@schouten.nl>
2024-11-28 21:38:54 +0100

    project done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,11 +4,12 @@ import { NavLink } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <section className="homepage">
+    <>
+      <section className="homepage"></section>
       <NavLink to="/createbag">
         <button className="btn start-btn">LET'S GET STARTED</button>
       </NavLink>
-    </section>
+    </>
   );
 };
 
