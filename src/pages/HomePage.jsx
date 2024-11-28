d79e26f916baa32ae9bcf7330623d4438d58c3d6
Peedee01 <piethein@schouten.nl>
2024-11-28 18:24:01 +0100

    merged

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,6 @@ import { NavLink } from "react-router-dom";
 
 const HomePage = () => {
   return (
-<<<<<<< HEAD
     <section className="home">
       <div className="homepage">
       </div>
@@ -18,15 +17,4 @@ const HomePage = () => {
 };
 
 export default HomePage;
-=======
-    <div>
-      <NavLink to="/createbag">createbag</NavLink>
-      <NavLink to="/detailspage">detailspage</NavLink>
-      <h1>Home</h1>
 
-    </div>
-  )
-}
-
-export default HomePage
->>>>>>> c6c1347b024f6d5a7bba460d831f9ed0a4a95bb9
